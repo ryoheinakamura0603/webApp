ebe660e360552ea2a34150701ed6d95cac47e7df
R_naka <ryoheinakamura0603@gmail.com>
2023-06-23 10:39:28 +0900

    Implement Tab switch UI

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask import render_template
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
+@app.route("/test")
+def test_hello():
+    return render_template("base.html")
+    
+
 @app.route("/")
 def hello_world():
     return "HELLO WORLD!"
-
 
 if __name__ == "__main__":
     app.debug = True
